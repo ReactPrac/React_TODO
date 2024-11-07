c4bd049ea9ab57a3dee797e4b33000d556e0cc75
silverywaves <babyshu1210@gmail.com>
2024-11-07 16:51:42 +0900

    Edit & Save & Delete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,38 +1,68 @@
-import React, {useState} from "react";
+import React, { useState, useRef } from "react";
 import AddTodo from "./AddTodo";
-import TodoList from "./TodoList"
+import TodoList from "./TodoList";
 
-function Todo(){
-    // 할 일 목록 저장
+function Todo() {
     const [todoList, setTodoList] = useState([]);
-
-    // 생성 ID
     const [idx, setIdx] = useState(0);
-    
-    // 입력값 저장
     const [inputValue, setInputValue] = useState("");
+    
+    // input에 대한 ref 생성
+    const inputRefs = useRef([]);
 
     // 할 일 추가
     const handleAddTodo = (e) => {
-        // Enter 키 입력 & 유효성 충족시 실행
-        if(e.keyCode === 13 && isValid(inputValue)){
+        if (e.key === 'Enter' && isValid(inputValue)) {
             const newTodo = {
-                id : idx + 1,           // 생성 ID
-                todo: inputValue,       // 할 일 내용
-                date: new Date(),       // 생성 날짜
-                isEdit: false,          // 수정 모드 여부
-                isDelete: false,        // 삭제 여부
-                isDone: false,          // 완료 여부
+                id: idx + 1,
+                todo: inputValue,
+                date: new Date(),
+                isEdit: false,
+                isDone: false,
             };
-            todoList.push(newTodo);     // 새로운 할 일 목록에 추가
-            // 최신 할 일 순서로 정렬
-            setTodoList(todoList.sort((a, b) => b.id - a.id).map(item => item));
-            setIdx(idx + 1)             // ID 업데이트
-            setInputValue("");          // 입력 필드 초기화
+            setTodoList([...todoList, newTodo].sort((a, b) => b.id - a.id));
+            setIdx(idx + 1);
+            setInputValue("");
         }
-    }
+    };
 
-    // 유효성 검사
+    // 수정
+    const handleEditTodo = (id) => {
+        const updatedList = todoList.map((item) => {
+            if (item.id === id) {
+                setInputValue(item.todo); // 기존 할 일 내용으로 inputValue 설정
+                return { ...item, isEdit: true }; // 수정 모드로 전환
+            }
+            return item;
+        });
+        setTodoList(updatedList);
+
+        // 수정 항목 자동 포커스
+        setTimeout(() => {
+            const index = todoList.findIndex(item => item.id === id);
+            if (inputRefs.current[index]) {
+                inputRefs.current[index].focus();
+            }
+        }, 0);  // 렌더링 이후 포커스 적용
+    };
+
+    // 수정 완료 후 저장
+    const handleSaveTodo = (id) => {
+        const updatedList = todoList.map((item) => {
+            if (item.id === id) {
+                return { ...item, todo: inputValue, isEdit: false };
+            }
+            return item;
+        });
+        setTodoList(updatedList);
+        setInputValue(""); // 입력 필드 초기화
+    };
+
+    // 삭제
+    const handleDeleteTodo = (id) => {
+        setTodoList(todoList.filter((item) => item.id !== id)); // 선택된 ID 제외
+    };
+
     const isValid = (todo) => todo !== "";
 
     return (
@@ -45,12 +75,18 @@ function Todo(){
             </header>
             <main>
                 <section className="layout-360">
-                    <TodoList 
+                    <TodoList
                         todoList={todoList}
+                        handleEditTodo={handleEditTodo}
+                        handleSaveTodo={handleSaveTodo}
+                        inputValue={inputValue}
+                        setInputValue={setInputValue}
+                        handleDeleteTodo={handleDeleteTodo}
+                        inputRefs={inputRefs}
                     />
                 </section>
             </main>
-            <AddTodo 
+            <AddTodo
                 inputValue={inputValue}
                 setInputValue={setInputValue}
                 handleAddTodo={handleAddTodo}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,43 @@ import React from "react";
 
 const TodoList = ({
     todoList,
+    handleEditTodo,
+    handleSaveTodo,
+    inputValue,
+    setInputValue,
+    handleDeleteTodo,
+    inputRefs,
 }) => {
     return (
         <div className="items">
-            {todoList.map((item) => (
-                <div key={item.id} className="item">
-                    <span>{item.todo}</span>
+            {todoList.map((item, index) => (
+                <div key={item.id}>
+                    <div className="fragment"></div>
+                    <input
+                        ref={(el) => inputRefs.current[index] = el}  // 각 input에 ref 설정
+                        className="todo form-control"
+                        value={item.isEdit ? inputValue : item.todo} // 수정 모드일 때 inputValue 사용
+                        onChange={(e) => setInputValue(e.target.value)} // 입력값 변경
+                        readOnly={!item.isEdit} // 수정 모드일 때만 입력 가능
+                        style={{
+                            color: item.isEdit ? "red" : "black", // 수정 모드일 때 색상 변경
+                        }}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                handleSaveTodo(item.id); // Enter키로 수정 저장
+                            }
+                        }}
+                    />
+                    <div className="edit">
+                        <a href="#!" onClick={() => handleEditTodo(item.id)}>
+                            <span className="material-symbols-outlined" style={{ color: item.isEdit ? "red" : "black" }}>
+                                edit
+                            </span>
+                        </a>
+                        <a href="#!" onClick={() => handleDeleteTodo(item.id)}>
+                            <span className="material-symbols-outlined" style={{ color: "black" }}>delete</span>
+                        </a>
+                    </div>
                 </div>
             ))}
         </div>
